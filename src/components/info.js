4020112b697d9e661212625ee4ae402c047b29a6
Анастасия Пеконина <anastasiapekonina@Air-Anastasia.mts.local>
2020-12-23 18:38:50 +0300

    done

---
+++ src/components/info.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Info extends React.Component{
+    render(){
+        return(
+            <div>
+                <h2>Узнайте погоду в своем городе</h2>
+            
+            </div>
+        )
+    }
+}
+export default Info;
